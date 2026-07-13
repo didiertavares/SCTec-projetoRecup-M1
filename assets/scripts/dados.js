36a4c064446863c1fefa4a5ea0172235f2f865cd
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-07-13 00:03:20 -0300

    feat/dados.js > em andamento: implementa função buscarVagas com fetch, async await, try/catch e checagem results

--- assets/scripts/dados.js
+++ assets/scripts/dados.js
@@ -18,3 +18,32 @@ function carregarVaga(){
 function salvarVaga(vaga){
     localStorage.setItem('vaga', JSON.stringify(vaga))
 }
+
+// ASYNC AWAIT & FETCH: draft, em andamento
+export async function buscarVagas(){
+    console.log('buscando vagas...')
+
+    // try/catch & resposta.ok: tratamento de erro
+    try{
+        const resposta = await fetch("./assets/dados/vagas.json")
+        
+        if (!resposta.ok){
+            throw new Error('servidor responde '+ resposta.status)
+        }
+        
+        const dados = await resposta.json()
+        
+        // checagem de results
+        if (!dados.results || dados.results.length === 0) {
+            return null
+        }
+        return dados
+    } catch (erro){
+        mostrarStatus('Não foi possível carregar! Tente novamente.', 'erro')
+    }
+}
+
+// Carregando (avisar que está pedindo)
+// Sucesso (mostrar)
+// Erro (mensagem amigável)
+// mensagem de erro é anunciada ao leitor de tela c/ aria-live (amarra A1 → A3: a acessibilidade volta viva)
